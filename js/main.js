d2fb835663f08ee045b3aad87767def847919815
Shachak Armon <shachak.armon@gmail.com>
2022-12-05 19:59:12 +0200

    Changes to the footer and my area. also checking the commit precedure

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
-console.log('Starting up');
+'use strict'
+
+console.log('Starting up')
+
+var gProjects =[
+    {
+        id: "sokoban",
+        name: "Sokoban",
+        title: "Gotta rearange here!",
+        desc: "Your goal is to push the crate to the alocated positions. You can push a box only if the next cell is empty. Good luck!",
+        url: "https://shachakar.github.io/Sokoban/"
+        publishedAt: ,
+        labels: ["matrixes", "keyboard events", "games"]
+    }, {
+        id: "mine-sweeper",
+        name: "Mine Sweeper",
+        title: "Find the mines",
+        desc: "Your goal is to find all the mines positions, and mark them with a flag (right click). if you hit a mine (left click) - you lose. Good luck!",
+        url: "https://shachakar.github.io/Mine-sweeper/"
+        publishedAt: ,
+        labels: ["matrixes", "keyboard events", "games"]
+
+    }
+]
+
